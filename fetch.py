c8176cea1cb0864b8fc339ab1828c04947e8a6a3
黄伯豪 <hbh19@mails.tsinghua.edu.cn>
2021-03-26 17:12:58 +0800

    column filter done

--- fetch.py
+++ fetch.py
@@ -1,28 +1,40 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 import numpy as np
 import pandas as pd
 from faker import Faker
-import json
+import ujson
+from flask_cors import CORS
 
 f=Faker(locale="en_CA")
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/hello')
 def hello_world():
-  return "Hello World!"
+  return jsonify(message="Hello World!")
 
 @app.route('/random')
 def random_data():
-  fake_features=np.random.rand(100,20)
-  fake_labels=[np.random.randint(0,2,30) for x in range(100)]
-  df=pd.DataFrame(data=fake_features,columns=[str("feature")+str(i+1) for i in range(20)])
+  row = int(request.args.get("row"))
+  features_no = int(request.args.get("features"))
+  labels_no = int(request.args.get("labels"))
+  fake_features=np.random.rand(row,features_no)
+  fake_labels=[np.random.randint(0,2,labels_no) for x in range(row)]
+  df=pd.DataFrame(data=fake_features,columns=[str("feature")+str(i+1) for i in range(features_no)])
   df["labels"]=fake_labels
-  return df.to_json(orient="split")
+  df1=pd.DataFrame(data=pd.concat([df.select_dtypes(['number']).min(), df.select_dtypes(['number']).max(),
+  df.median(),df.mean(),df.std(),df.var()],axis=1))
+  df1.columns=["min", "max", "median","mean","std","var"]
+  df1.to_json(orient="index")
+  df_decoded = ujson.loads(df.to_json(orient="table"))
+  df_decoded["stat"] = ujson.loads(df1.to_json(orient="index"))
+  return ujson.dumps(df_decoded), 200
 
 @app.route('/random/label')
 def random_label():
-  return json.dumps([f.word() for x in range(30)])
+  labels_no = int(request.args.get("labels"))
+  return ujson.dumps([f.word() for x in range(labels_no)]), 200
 
 if __name__ == "__main__":
   app.run()
